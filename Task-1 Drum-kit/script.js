e4005ce43f8916c0c9405cd84a6de5f6ab5f3309
UnzaShaikh <83079287+UnzaShaikh@users.noreply.github.com>
2022-10-21 00:35:21 +0500

    Update script.js

--- Task-1 Drum-kit/script.js
+++ Task-1 Drum-kit/script.js
@@ -1,38 +1,73 @@
-let keyvalues = [];
-for(let values=0;values<=9;values++){
-    keyvalues [values]= document.querySelectorAll(".key")[values].getAttribute('data-key');
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Nunito:wght@200;300;400;600;700&display=swap');
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Bebas Neue', cursive;
+    
 }
 
-let sound_values = [];
-for(let s_values=0;s_values<=9;s_values++){
-    sound_values [s_values]= document.querySelectorAll("audio")[s_values].getAttribute('data-key');
+body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    /* border: 2px solid rgb(228, 20, 20); */
+    width: 100%;
+    height: 100vh;
+    font-size: 2rem;
+    background-image: url(https://www.bing.com/images/blob?bcid=RFWeepGBIsoEqxcxoNWLuD9SqbotqVTdP4w);
+    background-position: center;
+    background-size: cover;
+    background-color: black;
 }
 
-let box = document.getElementsByClassName("box");
-// function style(){
-// for(var i=0;i<box.length;i++){
+
+/* keys-container styling */
+.keys{
+    display: flex;
+    /* border: 2px solid white; */
+    width: 100%;
+    height: 150px;
+    align-items: center;
+    justify-content: center;
+}
+
+.box{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 3px solid rgba(0, 0, 0, 0);
+    border-radius: 16px;
+    font-size: 2rem;
+    width: 90px;
+    height: 100px;
+    background-color: rgba(255, 255, 255, 0.074);
+    margin: 12px;
+    transition: all 0.07s;
+}
+
+.data{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+/* letter styling */
+.lettername{
+    color: white;
     
-//     box[i].style.borderColor="yellow";
-//   }
-// }
+}
+/* Span styling */
+.soundname{
+    font-size: 0.8rem;
+    color:rgb(236, 228, 14);
+    margin-bottom: 4px;
+}
 
-
-
-let counter = 0;
-
-let sound;
-
-
-window.addEventListener('keydown',(e)=>{
-    let counter=0;
-    for(counter;counter<=9;counter++){
-        if(keyvalues[counter]==e.keyCode){
-        console.log(sound = document.getElementsByClassName("audios")[counter].play());
-        sound.currentTime=0;
-        function styleset(){
-             box[counter].style.borderColor="yellow";
-        }
-        let mytimeout = setTimeout(styleset(),1000);
-    } 
-    
-}})
+/* .playing{
+    transform: scale(1.1);
+    border-color:rgb(14, 18, 236) ;
+    box-shadow: 0 0 10px #ffc600;
+} */
